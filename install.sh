e42f43461131b1956379d3b95123ebc577ac51ca
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-10-24 19:10:02 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+printf '\033[36m';echo script by thelearn-tech
+echo AKA Pritam Behera
+echo 
+echo automatically installation of speedtest-cli
+sleep 2
+cd
+curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+
+chmod +x speedtest-cli
+
+cp speedtest-cli speedtest
+
+mv speedtest /data/data/com.termux/files/usr/bin
+
+
+echo
+echo installation successful
+echo now type speedtest or ./speedtest-cli
+sleep 3
+
+cd
+rm -rf speedtest-cli
+echo
