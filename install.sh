f85ce82533ab92b76135dfdd3a24b43578d2a95b
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-11-20 02:36:51 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+printf '\033[36m';echo script by thelearn-tech
+echo AKA Pritam Behera
+echo
+echo automatically installation of speedtest-cli
+sleep 2
+cd
+curl -Lo speedtest-cli https://raw.githubusercontent.>
+echo
+echo installation successful
+echo now type ./speedtest
+sleep 3
+cd
+rm -rf speedtest-cli
+echo
