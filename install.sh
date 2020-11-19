c36f4ddf4f52f528364cadc6350e96af516db8c6
Pritam Behera <69230350+thelearn-tech@users.noreply.github.com>
2020-11-20 02:28:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,15 @@ echo automatically installation of speedtest-cli
 sleep 2
 cd
 curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-
 chmod +x speedtest-cli
-
 cp speedtest-cli speedtest
-
 mv speedtest /data/data/com.termux/files/usr/bin
-
-
+cd $PREFIX/bin
+chmod +x speedtest
 echo
 echo installation successful
 echo now type speedtest or ./speedtest-cli
 sleep 3
-
 cd
 rm -rf speedtest-cli
 echo
